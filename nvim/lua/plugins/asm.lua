2f8fe1d1aefac3054dfd74702f9eaddff13ada4a
Shane Dowley <shane.dowley@gmail.com>
2026-04-15 21:22:14 +0100

    Remove legacy top-level dotfiles duplicates

--- nvim/lua/plugins/asm.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/plugins/asm.lua
--- Z80 and assembly syntax support
-return {
-	{
-		"samsaga2/vim-z80", -- archived Z80 syntax highlighting
-		ft = { "z80", "asm" }, -- activate only for these filetypes
-	},
-}
